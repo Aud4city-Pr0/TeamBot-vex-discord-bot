d20151dfea5e522fec3f90e3ff32eafb6a7047cb
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2025-12-07 13:29:27 -0500

    automaticly actiave venv before installing packages

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 # vars
 VENV_PATH=".venv"
 PYPROJECT_FILE="requirements.txt"
+ACTIAVTE_BASH="$VENV_PATH/bin/activate"
 
 # package install function
 install_modules() {
@@ -21,10 +22,12 @@ install_modules() {
 # checking to see if it exists before we create a new one
 if [[ -d "$VENV_PATH" ]]; then
     echo "venv exists, moving on to installing packages"
+    source $ACTIAVTE_BASH
     install_modules
 else
     echo "venv does not exist, creating one now."
     python3 -m venv .venv
     echo "installing packages..."
+    source "$ACTIAVTE_BASH"
     install_modules
 fi
